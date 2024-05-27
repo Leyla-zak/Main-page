a410eab5979b014a1b12f7e42e93867d0a1e5837
adelinaxax <adelina1911@gmail.com>
2024-05-27 21:32:53 +0300

    z

--- myproject/authentication/models.py
+++ myproject/authentication/models.py
@@ -1,11 +1,10 @@
+import jwt
 from datetime import datetime, timedelta
-
+from django.db import models
 from django.conf import settings 
 from django.contrib.auth.models import (
 	AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
-
-from django.db import models
 
 class UserManager(BaseUserManager):
     """
