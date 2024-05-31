679bc6f4c07e15a7fef755d4f3fcfd07edc469ef
Leyla <zzzleyla3000@gmail.com>
2024-05-31 16:18:06 +0300

    пробую базу данных

--- myproject/inTime/models.py
+++ myproject/inTime/models.py
@@ -1,3 +1,14 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-all_users = User.objects.all()      
+class User(AbstractUser):
+    """Модель пользователя"""
+    pass
+
+class Bookmark(models.Model):
+    """Модель закладки"""
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookmarks")
