b46cd3aa08184d209986a72d606d8b9d6f78bddb
adelinaxax <adelina1911@gmail.com>
2024-06-02 21:33:45 +0300

    fastapi

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('task/create/', TaskCreate.as_view(),name='task-create'),
     path('task/update/<int:pk>/', TaskUpdate.as_view(),name='task-update'),
     path('task/delete/<int:pk>/', TaskDelete.as_view(),name='task-delete'),
+
+    path('api/', include('inTime.urls')), 
+    path('api-auth/', include('rest_framework.urls')),  
 ]
 
 if settings.DEBUG:
